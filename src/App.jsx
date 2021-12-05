72752dba8cd87cf76f2861aa0add4688f329b855
ogrodek <70914232+KrzysztofOGit@users.noreply.github.com>
2021-12-05 21:51:09 +0000

    Uplolad date from link

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
+import {useState, useEffect} from "react";
 import './App.css';
 //import './ShowQuote.jsx';
 
-const quotes = {
-  "Walt Disney": '"Jeśli możesz sobie coś wymarzyć, możesz to zrobić."',
-  "Nelson Mandela": '"Zawsze wydaje się, że coś jest niemożliwe, dopóki nie zostanie to zrobione."',
-  "Michael Jordan": '"Nie trafiłem ponad 9000 rzutów w moim życiu. Przegrałem ponad 300 meczów. 26 razu zaufano mi, gdy miałem oddać rzut na miarę zwycięstwa i spudłowałem. Przegrywałem w moim życiu ciągle. Dlatego właśnie osiągnąłem sukces."'
-}
+// const quotes = {
+//   "Walt Disney": '"Jeśli możesz sobie coś wymarzyć, możesz to zrobić."',
+//   "Nelson Mandela": '"Zawsze wydaje się, że coś jest niemożliwe, dopóki nie zostanie to zrobione."',
+//   "Michael Jordan": '"Nie trafiłem ponad 9000 rzutów w moim życiu. Przegrałem ponad 300 meczów. 26 razu zaufano mi, gdy miałem oddać rzut na miarę zwycięstwa i spudłowałem. Przegrywałem w moim życiu ciągle. Dlatego właśnie osiągnąłem sukces."'
+// }
+
+//nie sporawdzam czy plik istnieje
+
+
+
+
 
 function App() {
+	const [quotes, setQuotes] = useState([]);
+	useEffect(() => {
+		fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/')
+		.then((res) => res.json())
+		.then(data => setQuotes(data))
+		.catch(err => console.log(err))
+	},[]);
+	
 
 	function showQuote() {
+		let randomQuotes = Math.floor(Math.random() * quotes.length);
+		console.log(randomQuotes);
+		//var mydata = JSON.parse(quotes);
+		//console.log(this.state.quotes);
 		//utworzenie tablicy z autorami z obiektu quotes 
-		var autorzy = Object.keys(quotes);
+		//var autorzy = Object.keys(quotes);
 		//wylosowanie liczby z tablicy autorzy
-		var ranQuote = Math.floor(Math.random()* autorzy.length);
+		//var ranQuote = Math.floor(Math.random()* autorzy.length);
 		
-		var autor = autorzy[ranQuote];
-		var quote = quotes[autor];
+		//var autor = autorzy[ranQuote];
+		//var quote = quotes[autor];
 		
-		document.getElementById('quote').textContent = quote;
-		document.getElementById('autor').textContent = autor;
+		//document.getElementById('quote').textContent = quote;
+		//document.getElementById('author').textContent = autor;
 	}
 	
   return (
@@ -34,7 +53,7 @@ function App() {
           "Miejsce na cytat"
         </div>
         
-      <div class="autor" id="autor">
+      <div class="author" id="author">
           "Autor"
         </div>
       </div>
